219ee5c47ac3590505fb1bff65d8a75ade882942
Dylan <71920152+sansui-d@users.noreply.github.com>
2023-12-28 09:38:07 +0000

    init

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import Button from "@/components/Button.vue";
+</script>
+
+<template>
+  <div class="wrapper">
+    <Button text="按钮" />
+  </div>
+</template>
+
+<style scoped>
+.wrapper {
+  display: flex;
+}
+</style>
