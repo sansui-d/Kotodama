68cc40e1e507b56669ce1892160bef6617c1921d
Dylan <71920152+sansui-d@users.noreply.github.com>
2024-01-23 17:26:33 +0800

    code 1-23

--- src/components/CardWrapper.vue
+++ src/components/CardWrapper.vue
@@ -1,9 +1,7 @@
 <script setup lang="ts">
 import type { ICardProps } from '@/constants/types'
-defineProps<{cardData?:ICardProps}>();
-const handleCloseWrapper = () => {
-  
-}
+defineProps<{cardData?:ICardProps; onClose?: ()=>void }>();
+
 </script>
 
 <template>
@@ -11,11 +9,11 @@ const handleCloseWrapper = () => {
     class="card-warpper rounded-2xl bg-neutral-400 w-[50rem] h-[30rem] p-10 fixed top-2/4 left-2/4 
            translate-y-50 translate-x-50"
   >
-    <div class="close" @click="handleCloseWrapper"></div>
+    <div class="close" @click="onClose"></div>
     <div>{{ cardData?.index }}</div>
     <div>{{ cardData?.name }}</div>
-    <div>{{ cardData?.index }}</div>
-    <div>{{ cardData?.index }}</div>
+    <div>{{ cardData?.saying }}</div>
+    <div>{{ cardData?.introduction }}</div>
   </div>
 </template>
 
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,14 +1,23 @@
 <script setup lang="ts">
+import { ref, onMounted, onUnmounted } from 'vue'
 import { storeToRefs } from 'pinia'
 import CardList from '@/components/CardList.vue'
 import CardWrapper from '@/components/CardWrapper.vue'
 import { kotodama } from '@/constants/constants'
 import { useCardWrapperStore } from '@/stores/cardWrapperDataStore'
 import type { ICardProps } from '@/constants/types'
+
 const { cardData } = storeToRefs(useCardWrapperStore())
 const { setCardWrapperData } = useCardWrapperStore()
+const showCardWrapper = ref(false);
+
 const handleCardClick = (card?: ICardProps) => {
   setCardWrapperData({ ...card })
+  showCardWrapper.value = true
+}
+
+const handleCloseWrapper = () => {
+  showCardWrapper.value = false
 }
 </script>
 
@@ -17,7 +26,7 @@ const handleCardClick = (card?: ICardProps) => {
   <div class="card-list">
     <CardList :cardListData="kotodama" :onCardClick="handleCardClick" />
   </div>
-  <CardWrapper :cardData="cardData" />
+  <CardWrapper v-if="showCardWrapper" :cardData="cardData" :onClose="handleCloseWrapper" />
 </template>
 
 <style scoped></style>
