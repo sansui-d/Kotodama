7a2794d4adf8f370c0d8498c53058911eebb3554
Dylan <71920152+sansui-d@users.noreply.github.com>
2024-02-22 16:44:06 +0800

    switch

--- src/components/CardWrapper.vue
+++ src/components/CardWrapper.vue
@@ -1,23 +1,42 @@
 <script setup lang="ts">
+import { ref } from 'vue'
 import type { ICardProps } from '@/constants/types'
-defineProps<{cardData?:ICardProps; onClose?: ()=>void }>();
-
+import Switch from '@/components/Switch.vue'
+defineProps<{ cardData?: ICardProps; onClose?: () => void }>()
+const tab = ref(true)
+const onSwitchChange = () => {
+  tab.value = !tab.value
+}
 </script>
 
 <template>
-  <div
-    class="card-warpper rounded-2xl bg-neutral-400 w-[50rem] h-[30rem] p-10 fixed top-2/4 left-2/4 
-           translate-y-50 translate-x-50"
-  >
+  <div class="card-warpper rounded-2xl bg-neutral-400 w-[50rem]
+     h-[30rem] p-10 fixed top-2/4 left-2/4 translate-y-50 translate-x-50">
+    <div class="card-tab">
+      <Switch :leftText="'info'" :rightText="'pic'" :value="tab" :onChange="onSwitchChange" />
+    </div>
     <div class="close" @click="onClose"></div>
     <div>{{ cardData?.index }}</div>
     <div>{{ cardData?.name }}</div>
     <div>{{ cardData?.saying }}</div>
-    <div>{{ cardData?.introduction }}</div>
+    <div v-if="tab">{{ cardData?.introduction }}</div>
+    <img v-else src="../../public/favicon.png" />
   </div>
 </template>
 
 <style lang="less" scoped>
+.card-tab {
+  position: absolute;
+  top: 0.5rem;
+  left: 50%;
+  transform: translate(-50%);
+  display: flex;
+
+  &-info {
+    margin-right: 1rem;
+  }
+}
+
 .close {
   position: absolute;
   top: 0.5rem;
@@ -30,23 +49,26 @@ defineProps<{cardData?:ICardProps; onClose?: ()=>void }>();
   &:hover::after {
     background-color: aqua;
   }
+
   &:hover::before {
     background-color: aqua;
   }
 }
+
 .close::before,
 .close::after {
-    content: '';
-    width: 1px;
-    height: 100%;
-    background: #333;
-    display: block;
-    transform: rotate(45deg) translateX(0px);
-    position: absolute;
-    left: 50%;
-    top: 0;
+  content: '';
+  width: 1px;
+  height: 100%;
+  background: #333;
+  display: block;
+  transform: rotate(45deg) translateX(0px);
+  position: absolute;
+  left: 50%;
+  top: 0;
 }
+
 .close::after {
-    transform: rotate(-45deg) translateX(0px);
+  transform: rotate(-45deg) translateX(0px);
 }
 </style>
---
+++ src/components/Switch.vue
@@ -0,0 +1,136 @@
+<script setup lang="ts">
+defineProps<{
+    leftText: string;
+    rightText: string;
+    value: boolean;
+    onChange: void;
+}>();
+</script>
+
+<template>
+    <label class="switch">
+        <input class="cb" type="checkbox" :value="value" :onchange="onChange" />
+        <span class="toggle">
+            <span class="left">{{ leftText }}</span>
+            <span class="right">{{ rightText }}</span>
+        </span>
+    </label>
+</template>
+
+<style scoped>
+.switch {
+    font-size: 12px;
+    position: relative;
+    display: inline-block;
+    width: 5em;
+    height: 2.5em;
+    user-select: none;
+}
+
+.switch .cb {
+    opacity: 0;
+    width: 0;
+    height: 0;
+}
+
+.toggle {
+    position: absolute;
+    cursor: pointer;
+    width: 100%;
+    height: 100%;
+    background-color: #373737;
+    border-radius: 0.1em;
+    transition: 0.4s;
+    text-transform: uppercase;
+    font-weight: 700;
+    overflow: hidden;
+    box-shadow: -0.3em 0 0 0 #373737, -0.3em 0.3em 0 0 #373737,
+        0.3em 0 0 0 #373737, 0.3em 0.3em 0 0 #373737, 0 0.3em 0 0 #373737;
+}
+
+.toggle>.left {
+    font-size: 10px;
+    position: absolute;
+    display: flex;
+    width: 50%;
+    height: 88%;
+    background-color: #f3f3f3;
+    color: #373737;
+    left: 0;
+    bottom: 0;
+    align-items: center;
+    justify-content: center;
+    transform-origin: right;
+    transform: rotateX(10deg);
+    transform-style: preserve-3d;
+    transition: all 150ms;
+}
+
+.left::before {
+    position: absolute;
+    content: "";
+    width: 100%;
+    height: 100%;
+    background-color: rgb(206, 206, 206);
+    transform-origin: center left;
+    transform: rotateY(90deg);
+}
+
+.left::after {
+    position: absolute;
+    content: "";
+    width: 100%;
+    height: 100%;
+    background-color: rgb(112, 112, 112);
+    transform-origin: center bottom;
+    transform: rotateX(90deg);
+}
+
+.toggle>.right {
+    font-size: 10px;
+    position: absolute;
+    display: flex;
+    width: 50%;
+    height: 88%;
+    background-color: #f3f3f3;
+    color: rgb(206, 206, 206);
+    right: 1px;
+    bottom: 0;
+    align-items: center;
+    justify-content: center;
+    transform-origin: left;
+    transform: rotateX(10deg) rotateY(-45deg);
+    transform-style: preserve-3d;
+    transition: all 150ms;
+}
+
+.right::before {
+    position: absolute;
+    content: "";
+    width: 100%;
+    height: 100%;
+    background-color: rgb(206, 206, 206);
+    transform-origin: center right;
+    transform: rotateY(-90deg);
+}
+
+.right::after {
+    position: absolute;
+    content: "";
+    width: 100%;
+    height: 100%;
+    background-color: rgb(112, 112, 112);
+    transform-origin: center bottom;
+    transform: rotateX(90deg);
+}
+
+.switch input:checked+.toggle>.left {
+    transform: rotateX(10deg) rotateY(45deg);
+    color: rgb(206, 206, 206);
+}
+
+.switch input:checked+.toggle>.right {
+    transform: rotateX(10deg) rotateY(0deg);
+    color: #487bdb;
+}
+</style>
