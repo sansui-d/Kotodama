adb5bbca18d2581cf852184aef1d986ec084cf33
dengchuandong <sansui1021@163.com>
2024-06-04 23:08:35 +0800

    code 6-4

--- src/pages/Product/Detail/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-function Detail() {
-    return (
-        <div>产品详情</div>
-    );
-}
-
-export default Detail;
--- src/pages/Product/List/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-function List() {
-    return (
-        <div>产品列表</div>
-    );
-}
-
-export default List;
